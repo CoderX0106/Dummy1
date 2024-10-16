b066e7957f6272d3342f709faa7419bba52deaa3
Siddhant Jadhav <siddhantjadhav0106@gmail.com>
2024-10-16 15:42:36 +0530

    first commit

---
+++ VS-CODE/PYTHON/task.py
@@ -0,0 +1,27 @@
+# n=6
+# z='mangoes'
+# p=125.51234
+
+# print('i have brought %d,%s in Rs %.2f'%(n,z,p))
+# print(f"i have brought {n} , {z} in RS {p:.2f}")
+# print("i have brpught {} {} in rs {:.2f}".format(n,z,p))
+
+# num1=int(input("enter a number: "))
+# match num1:
+#     case 1:
+#         print("one")
+#     case 2:
+#         print("two")
+#     case 3:
+#         print("three")
+#     case _:
+#         print("number not between 1 and 3")
+
+# ch2=input("enter a charecter :")
+# match ch2:
+#     case list1:
+#         print(f"{ch2} is a vowel")
+#     case _:
+#         print(f"{ch2} is not a vowel")
+
+
